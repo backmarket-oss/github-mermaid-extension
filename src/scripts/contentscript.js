e327dd162a79570a03229404029595898ad2b70b
Alex Mercier <pro.alexandre.mercier@gmail.com>
2019-04-25 18:29:47 +0200

    Initial implementation

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,4 +1,6 @@
-import ext from "./utils/ext";
+import 'babel-polyfill'
+import ext from "./utils/ext"
+import mermaidAPI from 'mermaid'
 
 var extractTags = () => {
   var url = document.location.href;
@@ -32,3 +34,96 @@ function onRequest(request, sender, sendResponse) {
 }
 
 ext.runtime.onMessage.addListener(onRequest);
+
+// ---
+
+/**
+ * Unique IDs generator.
+ *
+ * @generator
+ * @function idGenerator
+ * @yields {string} A unique id.
+ */
+function *idGenerator() {
+  for (let i = 0; true; i++) {
+    yield `github-mermaid-extension-${i}`
+  }
+}
+
+/**
+ * Render a Mermaid diagram.
+ * @param {string} code Diagram source code, in Mermaid language.
+ * @param {HTMLDivElement} target Element to insert <svg> into.
+ * @param {string} id A unique element id.
+ */
+function render(code, target, id) {
+  mermaidAPI.render(id, code, (svgCode, bindFunctions) => {
+    target.innerHTML = svgCode
+    bindFunctions(target)
+  }, target)
+}
+
+/**
+ * Render a Mermaid error.
+ * @param {string} message Mermaid error message.
+ * @param {HTMLDivElement} target Element to insert error message into.
+ */
+function renderError(message, target) {
+  target.style.color = 'red'
+  target.innerHTML = `
+    <h4>Mermaid syntax error</h4>
+    <pre>${message}</pre>
+  `
+}
+
+/**
+ * Generate a Mermaid diagram into a new <div>
+ * @param {string} source Element containing the diagram source code, in Mermaid language.
+ * @param {string} id A unique element id. 
+ * @returns {HTMLDivElement} The new <div> element.
+ */
+function processElement(source, id) {
+  const target = document.createElement('div')
+  source.after(target) // Note: must happen before render
+  try {
+    render(source.textContent, target, id)
+    source.style.display = 'none'
+  } catch (error) {
+    renderError(error.message, target)
+    throw error
+  }
+  return target
+}
+
+/**
+ * Process all `pre[lang="mermaid"]` elements that don't have
+ * `[data-processed="true"]`.
+ * @param {Iterator<string>} idIterator Id iterator
+ */
+function processUnprocessedElements(idIterator) {
+  const selector = 'pre[lang="mermaid"]:not([data-processed])';
+  [...document.querySelectorAll(selector)].forEach(source => {
+    source.setAttribute('data-processed', 'true')
+    const target = processElement(source, idIterator.next().value)
+  })
+}
+
+// Mermaid setup
+// Read more: https://mermaidjs.github.io/mermaidAPI.html#configuration
+mermaidAPI.initialize({
+  startOnLoad: false
+})
+
+const globalIdIterator = idGenerator()
+
+// Observe nodeList and attribute changes on the page, and
+// process all unprocessed elements on the page.
+new MutationObserver(
+  () => processUnprocessedElements(globalIdIterator)
+).observe(
+  document.body,
+  { childList: true, subtree: true }
+)
+
+// Initial pass
+processUnprocessedElements(globalIdIterator)
